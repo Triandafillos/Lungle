98adc077c8e64883f0b57126c718fd68ab0e17ee
Triandafillos <georgepapadakis1111@gmail.com>
2026-07-27 06:27:29 -0600

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Outlet />
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+    <div className="p-1 ml-auto flex items-center text-lg">
+        <a href="https://georgepapadakis.com" target="_blank" className="flex items-center gap-2">
+            <img src="gplogo.png" className="w-8 h-8" />
+            <p>This project is part of George Papadakis's portfolio.</p>
+        </a>
+    </div>
+</footer>
+  )
+}
